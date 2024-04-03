5e230c73ba2080aa0811042404f3783cc95ea201
Kristyna Podana <kristyna.zam@gmail.com>
2024-04-03 11:07:58 +0200

    Pridana komponenta ReservationPage

---
+++ src/components/ReservationPage/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const ReservationPage = () => {
+  return (
+    <div>
+      <h2>Stránka s rezervací</h2>
+    </div>
+  );
+};
--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,24 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
 import { App } from './components/App';
+import { HomePage } from './pages/HomePage/index.jsx';
+import { ReservationPage } from './components/ReservationPage/index.jsx';
 import './global.css';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: '/reservation',
+        element: <ReservationPage />,
+      },
+    ]
   },
 ]);
 
