ed3a4f78ea2813e1cf802e47571e29bbabd2f8a4
Kristyna Podana <kristyna.zam@gmail.com>
2024-04-03 20:24:08 +0200

    Pridano zobrazeni sedadla

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { JourneyPicker } from '../../components/JourneyPicker';
 import { JourneyDetail } from '../../components/JourneyDetail/index.jsx';
+import { SelectedSeat } from '../../components/SelectedSeat/index.jsx';
 
 export const HomePage = () => {
   const [journey, setJourney] = useState(null);
@@ -15,6 +16,9 @@ export const HomePage = () => {
       <JourneyPicker onJourneyChange={handleJourneyChange}/>
       {journey && (
         <JourneyDetail journey={journey}/>
+      )}
+      {journey && (
+      <SelectedSeat number={journey.autoSeat} />
       )}
     </main>
   );
